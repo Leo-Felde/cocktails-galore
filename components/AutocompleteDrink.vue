f62bb30f9aa548dfaa8276f0d132bc684e30d72a
Leonardo H. Felde <leonardohfelde@gmail.com>
2023-08-05 11:15:46 -0300

    feat: autocomplete de bebida

---
+++ components/AutocompleteDrink.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <v-autocomplete
+      v-model="selectedDrink"
+      placeholder="search a drink"
+      prepend-inner-icon="mdi-magnify"
+      item-title="strDrink"
+      item-value="idDrink"
+      density="compact"
+      rounded
+      variant="solo"
+      hide-details
+      :items="drinksFound"
+      :loading="loading"
+      @update:search="searchForDrinks"
+      @update:modelValue="selectDrink"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { ref } from 'vue'
+
+import CocktailAPI from '~/api/CocktailsBD'
+
+interface Drink {
+  strDrink: string,
+  strDrinkThumb: string,
+  idDrink: string
+}
+
+export default {
+  name: 'AutocompleteDrink',
+
+  setup () {
+    const drinksFound = ref([])
+    const selectedDrink = ref<string>('')
+    const loading = ref(false)
+    
+    const searchForDrinks = async (test: string) => {
+      if (test === '') {
+        drinksFound.value = []
+        return
+      }
+      try {
+        loading.value = true
+        const resp = await CocktailAPI.searchByName(test)
+        console.log(resp)
+        drinksFound.value = resp.data.drinks
+      } catch (error) {
+        // console.error('Error trying to get the drink:', error)
+        throw error
+      } finally {
+        loading.value = false
+      }
+    }
+
+    const selectDrink = async () => {
+      console.log(selectedDrink.value)
+      if (selectedDrink.value) {
+        navigateTo({path: `/drink-${selectedDrink.value}`})
+        selectedDrink.value = null
+      }
+    }
+
+    return {
+      loading,
+      selectedDrink,
+      drinksFound,
+      searchForDrinks,
+      selectDrink
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+:deep(.v-field__field)
+  height: 35px !important
+:deep(.v-field__input)
+  padding-top: 5px
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -10,29 +10,31 @@
           <v-icon> mdi-liquor </v-icon>
         </span>
 
-        <v-spacer></v-spacer>
+        <autocomplete-drink class="search" v-if="!mobile" />
 
-        <v-menu>
-          <template v-slot:activator="{ props }">
-            <span v-bind="props">
-              My favourites
-              <v-icon> mdi-chevron-down </v-icon>
-            </span>
-          </template>
-          <v-list>
-            <v-list-item
-              v-for="(drink, index) in favouriteDrinks"
-              :key="index"
-              :value="index"
-            >
-              <v-list-item-title>
-                {{ drink.name }}
-              </v-list-item-title>
-            </v-list-item>
-          </v-list>
-        </v-menu>
+        <div class="app-bar-content__actions">
+          <v-menu>
+            <template v-slot:activator="{ props }">
+              <span v-bind="props">
+                My favourites
+                <v-icon> mdi-chevron-down </v-icon>
+              </span>
+            </template>
+            <v-list>
+              <v-list-item
+                v-for="(drink, index) in favouriteDrinks"
+                :key="index"
+                :value="index"
+              >
+                <v-list-item-title>
+                  {{ drink.name }}
+                </v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
 
-        <theme-button @toggleTheme="toggleTheme"/>
+          <theme-button @toggleTheme="toggleTheme"/>
+        </div>
       </div>
     </v-card>
       
@@ -44,7 +46,7 @@
 </template>
 
 <script lang="ts">
-import { useTheme } from 'vuetify'
+import { useTheme, useDisplay } from 'vuetify'
 
 export default {
   name: 'DefaultLayout',
@@ -52,10 +54,12 @@ export default {
   setup () {
     const favouriteDrinks = ref([{name: 'beer'}])
     const theme = useTheme()
+    const { mobile } = useDisplay()
 
     return {
       theme,
       favouriteDrinks,
+      mobile,
       toggleTheme: () => theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
     }
   }
@@ -72,5 +76,17 @@ export default {
   &-content
     margin-top: auto
     margin-bottom: auto
-    flex-grow: 1
+    display: flex
+    align-items: center
+    width: 100%
+    height: inherit
+    margin-left: 10px
+    margin-right: 10px
+    &__actions
+      display: flex
+      margin-left: auto
+
+.search
+  width: 300px !important
+  margin-left: 10px
 </style>
